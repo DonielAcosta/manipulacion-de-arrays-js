c4987fec690e5df4ca30b49c148ee6425435f722
doniel <donielacosta1995@gmail.com>
2023-11-21 19:39:27 -0400

    reduce

---
+++ server/reduce.js
@@ -0,0 +1,12 @@
+const total = [1,2,3,4,5,6,7,8,4,2,4];
+
+let sum = 0;
+
+for (let index = 0; index < total.length; index++) {
+    const element = total[index];
+    sum = sum + element;
+}
+
+console.log(sum);
+
+const respuesta = total.reduce();
